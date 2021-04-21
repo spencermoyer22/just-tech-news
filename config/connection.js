f876ed7e393fc21cb906ff034b04618f1c04e359
Big Head <spencer.moyer16@yahoo.com>
2021-04-20 18:37:44 -0700

    created Post object

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-const sequelize = new Sequelize('just_tech_news_db', 'root', 'naruto', {
+const sequelize = new Sequelize('just_tech_news_db', 'root', 'm0m0nkey1010', {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
